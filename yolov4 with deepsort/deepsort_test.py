2d64fed36bf0fae62d99a19dbbf3ea0e2371c193
Sanjay Subramanya <54275083+sanjay-subramanya@users.noreply.github.com>
2022-07-05 22:31:24 +0530

    Add files via upload

---
+++ yolov4 with deepsort/deepsort_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  9 13:36:43 2022
+
+@author: Sanjay
+"""
+
+
+# %cd D:\yolov4-deepsort
+
+# 24 sec video
+
+!python D:/yolov4-deepsort/object_tracker.py --video D:/yolov4-deepsort/data/video/test24.mp4 --output D:/yolov4-deepsort/outputs/test24_output.avi --model yolov4 --info
